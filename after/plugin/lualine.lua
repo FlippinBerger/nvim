0d9b5df417e58eb293093f36839521fa7d1ad831
Chris Berger <flippinberger@gmail.com>
2024-10-09 14:48:57 -0600

    Add filename with parent dir to help differentiate in next.js

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,28 +1,28 @@
 require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '|', right = '|'},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
+    options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = { left = '|', right = '|' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+        }
     },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
-  },
-  sections = {
-    lualine_a = {'buffers'},
-    lualine_b = {''},
-    lualine_c = {''},
-    lualine_x = {'branch', 'diff'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
+    sections = {
+        lualine_a = { { 'filename', file_status = true, path = 4 } },
+        lualine_b = { 'buffers' },
+        lualine_c = { '' },
+        lualine_x = { 'branch', 'diff' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
 }
